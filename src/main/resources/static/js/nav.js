684245860455d8d3d35d6ff19cee4d0ba10ec529
masche14 <zskfnxh134@gmail.com>
2025-04-25 10:00:06 +0900

    변경

---
+++ src/main/resources/static/js/nav.js
@@ -0,0 +1,5 @@
+if (SS_USER_ID && SS_USER_ID.trim() != null) {
+    document.getElementById("beforeLoginNav").style.display = "none";
+} else {
+    document.getElementById("afterLoginNav").style.display = "none";
+}
