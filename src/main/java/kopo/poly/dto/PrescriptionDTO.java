c66247c6b09f267fae17cb18e8537b7f3322521f
masche14 <zskfnxh134@gmail.com>
2025-05-13 12:32:35 +0900

    변경

--- src/main/java/kopo/poly/dto/PrescriptionDTO.java
+++ src/main/java/kopo/poly/dto/PrescriptionDTO.java
@@ -1,11 +1,13 @@
 package kopo.poly.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.bson.types.ObjectId;
 
 import java.time.LocalDate;
 import java.util.*;
 
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 @Data
 public class PrescriptionDTO {
     private String prescriptionId;
