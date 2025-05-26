89b6d1f1660e5b8ebadf9e1578c8834ace400e24
masche14 <zskfnxh134@gmail.com>
2025-05-26 22:44:57 +0900

    s

--- src/main/java/kopo/poly/persistance/mongodb/IMongoMapper.java
+++
@@ -1,8 +0,0 @@
-package kopo.poly.persistance.mongodb;
-
-import kopo.poly.dto.MongoDTO;
-
-public interface IMongoMapper {
-
-    int insertData(MongoDTO pDTO, String colNm) throws Exception;
-}
--- src/main/java/kopo/poly/service/IMongoService.java
+++
@@ -1,8 +0,0 @@
-package kopo.poly.service;
-
-import kopo.poly.dto.MongoDTO;
-
-public interface IMongoService {
-
-    int mognoTest(MongoDTO pDTO) throws Exception;
-}
