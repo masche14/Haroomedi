13f292d137e104b872f4212f77764fdc476239b9
masche14 <zskfnxh134@gmail.com>
2025-08-21 13:16:13 +0900

    중복확인 js 수정

--- src/main/java/kopo/poly/HaroomediApplication.java
+++ src/main/java/kopo/poly/HaroomediApplication.java
@@ -3,9 +3,11 @@ package kopo.poly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableScheduling // ✅ 반드시 필요!
+@EnableFeignClients(basePackages = "kopo.poly.client") // Feign 인터페이스 패키지
 public class HaroomediApplication {
 
     public static void main(String[] args) {
